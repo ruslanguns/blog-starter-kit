9831472215755304cec63962be85bc45987c5eb5
Ruslan Gonzalez <ruslanguns@gmail.com>
2021-01-11 15:48:08 +0000

    feat: added blog with ng g @scullyio/init:blog

--- scully.blog-starter-kit.config.ts
+++ scully.blog-starter-kit.config.ts
@@ -4,5 +4,27 @@ export const config: ScullyConfig = {
   projectName: "blog-starter-kit",
   outDir: './dist/static',
   routes: {
+    '/blog/:slug': {
+      type: 'contentFolder',
+      slug: {
+        folder: "./blog"
+      }
+    },
+  },
+  puppeteerLaunchOptions: {
+    args: [
+      "--disable-gpu",
+      "--renderer",
+      "--no-sandbox",
+      "--no-service-autorun",
+      "--no-experiments",
+      "--no-default-browser-check",
+      "--disable-dev-shm-usage",
+      "--disable-setuid-sandbox",
+      "--no-first-run",
+      "--no-zygote",
+      "--single-process",
+      "--disable-extensions"
+    ]
   }
 };
